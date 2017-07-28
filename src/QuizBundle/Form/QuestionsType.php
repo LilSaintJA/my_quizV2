5caf0a21b45a4802fc7aef7028d534002b91b3fc
LilSaint <pamela.carric@gmail.com>
2017-07-28 19:30:55 +0200

    Ajout de la relation question/reponses

---
+++ src/QuizBundle/Form/QuestionsType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace QuizBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class QuestionsType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('questions')->add('idTheme')->add('theme');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'QuizBundle\Entity\Questions'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'quizbundle_questions';
+    }
+
+
+}
---
+++ src/QuizBundle/Form/ResponsesType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace QuizBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ResponsesType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nom')->add('idQuestion')->add('status')->add('question');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'QuizBundle\Entity\Responses'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'quizbundle_responses';
+    }
+
+
+}
